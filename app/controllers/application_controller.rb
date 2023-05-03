3223050336d328e90e35ce99903c42bee000f071
haliljon <juraboevhaliljon@gmail.com>
2023-05-03 18:04:12 +0500

    Add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
-   protect_from_forgery with: :exception
-   
-   before_action :update_allowed_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
-   protected
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password)}
-    
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password)}
-        end
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:full_name, :email, :password, :current_password)
+    end
+  end
 end

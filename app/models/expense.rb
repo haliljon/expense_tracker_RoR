5ad0f84ff181de5c5fb6a7e2fe68bf6dcbbe4abf
haliljon <juraboevhaliljon@gmail.com>
2023-05-05 15:37:02 +0500

    Fix linters

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,5 +3,5 @@ class Expense < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
-  validates :amount, presence: true, numericality: { greater_than: 0 } 
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end

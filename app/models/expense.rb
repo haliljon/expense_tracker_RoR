9aaf55691332fa9fdd0f91af9bed13e5485a8007
haliljon <juraboevhaliljon@gmail.com>
2023-05-05 15:32:36 +0500

    Add style

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :amount, presence: true, numericality: { greater_than: 0 } 
 end
